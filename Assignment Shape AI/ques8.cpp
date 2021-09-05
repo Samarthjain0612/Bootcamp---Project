8f94864133113cfe68d89787e244cba58c7f9fc1
Samarthjain0612 <84933489+Samarthjain0612@users.noreply.github.com>
2021-09-05 12:26:18 +0530

    DSA with C++ Project Submitted

---
+++ Assignment Shape AI/ques8.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+int main()
+ {
+    vector<int> gradingstudent (vector<int>v){
+        int n=v.size();
+        for (int i=0;i<n;i++){
+            int qt=v[i]/5;
+            int divf=qt*5;
+            int nextf=(divf+5);
+            if(divf<v[i]){
+                int diff=(nrxtf-v[i]);
+                if(diff<3&&nextf >=40)[
+                    v[i]=(v[i]+diff);
+                ]
+            }
+        }
+    }
+    return v;
+ }
+ int main()
+ {
+     oftream fourt getenv("OUTPUT_PATH");
+ }
+ 
